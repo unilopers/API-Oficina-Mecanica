e1607340e5c0affaa4f65defa2976db7ff8601ec
WlFerrari <ferrarijvt@gmail.com>
2026-03-09 23:41:52 -0300

    feat: adicionar entidade user, repository e configuracao de encoder

---
+++ src/main/java/com/unifil/oficinaMecanica/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.unifil.oficinaMecanica.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
